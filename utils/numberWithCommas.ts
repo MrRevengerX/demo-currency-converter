1f59edf1562c2a2142fe887562e5028589deadb7
MrRevengerX <ravindusenruwan@gmail.com>
2025-03-08 21:01:51 +0530

    Refactor react numbers package

--- utils/numberWithCommas.ts
+++ utils/numberWithCommas.ts
@@ -1,4 +1,10 @@
+import { numericFormatter } from "react-number-format";
+
 // Regex to add commas to numbers
-export function numberWithCommas(x: number) {
-  return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+export function numberWithCommas(x: string) {
+  return numericFormatter(x, {
+    thousandSeparator: true,
+    decimalScale: 3,
+    allowNegative: false,
+  });
 }
